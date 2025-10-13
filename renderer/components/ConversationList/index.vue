f8ce16ac8b13fbb45aa2b807db44be263bd3a032
EricWXY <ericwxy@foxmail.com>
2025-10-13 15:31:58 +0800

    feat(ConversationList): 添加对话标题溢出处理功能
    实现对话标题在宽度不足时显示省略号并添加悬浮提示
    新增ItemTitle组件处理标题溢出检测
    通过上下文注入侧边栏宽度实现响应式布局

--- renderer/components/ConversationList/index.vue
+++ renderer/components/ConversationList/index.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
 import { useFilter } from './useFilter';
+import { CTX_KEY } from './constants';
 import SearchBar from './SearchBar.vue';
 import ListItem from './ListItem.vue';
 
 defineOptions({ name: 'ConversationList' });
 
+const props = defineProps<{ width: number }>();
+
 const { conversations } = useFilter();
+
+provide(CTX_KEY, {
+  width: computed(() => props.width),
+});
 </script>
 
 <template>
-  <div class="conversation-list px-2 pt-3 h-[100vh] flex flex-col">
+  <div class="conversation-list px-2 pt-3 h-[100vh] flex flex-col" :style="{ width: 'calc(100% - 57px)' }">
     <search-bar class="mt-3" />
     <ul class="flex-auto overflow-auto">
       <template v-for="item in conversations" :key="item.id">
